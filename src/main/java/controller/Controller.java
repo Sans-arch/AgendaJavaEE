93075af252689be36c65c5c939bb1ef04c4a9cc7
Santiago Fleitas <sansfleitas@gmail.com>
2022-07-15 17:33:32 -0300

    creating DAO

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -6,10 +6,13 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.DAO;
 
 @WebServlet(urlPatterns = { "/Controller", "/main" }) // main é uma requisição que o servlet irá trabalhar
 public class Controller extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
+	private DAO dao = new DAO();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -23,7 +26,6 @@ public class Controller extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
--- src/main/java/model/DAO.java
+++ src/main/java/model/DAO.java
@@ -1,5 +1,29 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+/**
+ * Módulo de conexão
+ */
+public class DAO {
+	
+	// Parâmetros de conexão
+	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String url = "jdbc:mysql://127.0.0.1:3306/dbagenda?useTimezone=true&serverTimezone=UTC";
+	private String user = "root";
+	private String password = "K@tharsis19";
+	
+	// Método de conexão
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
 }
